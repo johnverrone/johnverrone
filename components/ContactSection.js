47947586de26f544ebfcf8900216160c4745d0f4
John Verrone <john.verrone@gmail.com>
2018-01-26 16:17:07 -0500

    working on the contact page:

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,10 +1,39 @@
-import SectionHeader from './SectionHeader';
+import SectionHeader from "./SectionHeader";
+import { Grid, GridItem } from './Grid';
+import FontAwesome from 'react-fontawesome';
 
 export default () => (
   <section id="contact">
     <SectionHeader>Contact</SectionHeader>
-    <div className="container center">
-      <p>contact me</p>
-    </div>
+    <Grid className="cl-effect-21">
+      <GridItem>
+        <FontAwesome name="instagram" size="2x" />
+        <a
+          href="https://www.linkedin.com/in/johnverrone/"
+          target="_blank"
+          className="socialButton"
+          id="linkedinButton"
+        />
+        <h6>/in/johnverrone</h6>
+      </GridItem>
+      <GridItem>
+        <a
+          href="http://twitter.com/johnverrone"
+          target="_blank"
+          className="socialButton"
+          id="twitterButton"
+        />
+        <h6>@johnverrone</h6>
+      </GridItem>
+      <GridItem>
+        <a
+          href="mailto:john.verrone@gmail.com"
+          target="new"
+          className="socialButton"
+          id="mailButton"
+        />
+        <h6>john.verrone@gmail.com</h6>
+      </GridItem>
+    </Grid>
   </section>
 );
