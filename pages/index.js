9a2d2bc352020f0e52fd207e9ac322d98d11b951
John Verrone <john.verrone@gmail.com>
2018-01-30 09:37:08 -0500

    removing commented about section

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Layout from "../components/Layout";
 import HomeSection from "../components/HomeSection";
 import WorkSection from "../components/WorkSection";
-import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
 
 export default () => (
   <Layout removePadding={true}>
     <HomeSection />
     <WorkSection />
-    {/* <AboutSection /> */}
     <ContactSection />
   </Layout>
 );
